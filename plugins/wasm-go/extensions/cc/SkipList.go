ca9c5c14e9e16a1337e1223b315ba91a6d2b01c6
zhoulaiqing <zhoulaiqing@hotmail.com>
2023-07-27 22:44:27 +0800

    添加 zset 实现

--- plugins/wasm-go/extensions/cc/SkipList.go
+++ plugins/wasm-go/extensions/cc/SkipList.go
@@ -8,8 +8,7 @@ import (
 const maxLevel = 16 // SkipList 的最大层数
 
 type node struct {
-	key     int
-	value   interface{}
+	content int64
 	forward []*node
 }
 
@@ -20,17 +19,16 @@ type SkipList struct {
 }
 
 // 创建一个新的节点
-func createNode(key int, value interface{}, level int) *node {
+func createNode(content int64, level int) *node {
 	return &node{
-		key:     key,
-		value:   value,
+		content: content,
 		forward: make([]*node, level),
 	}
 }
 
 // NewSkipList 创建一个新的 SkipList
 func NewSkipList() *SkipList {
-	head := createNode(math.MinInt64, nil, maxLevel)
+	head := createNode(math.MinInt64, maxLevel)
 	return &SkipList{
 		head:   head,
 		level:  1,
@@ -48,13 +46,13 @@ func randomLevel() int {
 }
 
 // Insert 插入节点到 SkipList 中
-func (sl *SkipList) Insert(key int, value interface{}) {
+func (sl *SkipList) Insert(content int64) {
 	update := make([]*node, maxLevel)
 	current := sl.head
 
 	// 寻找每层插入位置
 	for i := sl.level - 1; i >= 0; i-- {
-		for current.forward[i] != nil && current.forward[i].key < key {
+		for current.forward[i] != nil && current.forward[i].content < content {
 			current = current.forward[i]
 		}
 		update[i] = current
@@ -71,7 +69,7 @@ func (sl *SkipList) Insert(key int, value interface{}) {
 	}
 
 	// 创建新节点，并进行连接
-	newNode := createNode(key, value, level)
+	newNode := createNode(content, level)
 	for i := 0; i < level; i++ {
 		newNode.forward[i] = update[i].forward[i]
 		update[i].forward[i] = newNode
@@ -80,18 +78,18 @@ func (sl *SkipList) Insert(key int, value interface{}) {
 	sl.length++
 }
 
-// 查找指定 key 的节点
-func (sl *SkipList) searchNode(key int) *node {
+// 查找指定 content 的节点
+func (sl *SkipList) searchNode(content int64) *node {
 	current := sl.head
 
 	for i := sl.level - 1; i >= 0; i-- {
-		for current.forward[i] != nil && current.forward[i].key < key {
+		for current.forward[i] != nil && current.forward[i].content < content {
 			current = current.forward[i]
 		}
 	}
 
-	// 找到 key 对应的节点或者该节点不存在
-	if current.forward[0] != nil && current.forward[0].key == key {
+	// 找到 content 对应的节点或者该节点不存在
+	if current.forward[0] != nil && current.forward[0].content == content {
 		return current.forward[0]
 	}
 
@@ -99,20 +97,20 @@ func (sl *SkipList) searchNode(key int) *node {
 }
 
 // RangeSearch 查找在范围 [start, end] 内的节点，并返回它们的值
-func (sl *SkipList) RangeSearch(start, end int) []interface{} {
-	var result []interface{}
+func (sl *SkipList) RangeSearch(start, end int64) []int64 {
+	var result []int64
 	current := sl.head
 
 	for i := sl.level - 1; i >= 0; i-- {
-		for current.forward[i] != nil && current.forward[i].key < start {
+		for current.forward[i] != nil && current.forward[i].content < start {
 			current = current.forward[i]
 		}
 	}
 
 	current = current.forward[0]
 
-	for current != nil && current.key <= end {
-		result = append(result, current.value)
+	for current != nil && current.content <= end {
+		result = append(result, current.content)
 		current = current.forward[0]
 	}
 
@@ -120,19 +118,19 @@ func (sl *SkipList) RangeSearch(start, end int) []interface{} {
 }
 
 // RangeCount 计算在范围 [start, end] 内的节点个数
-func (sl *SkipList) RangeCount(start, end int) int {
+func (sl *SkipList) RangeCount(start, end int64) int {
 	count := 0
 	current := sl.head
 
 	for i := sl.level - 1; i >= 0; i-- {
-		for current.forward[i] != nil && current.forward[i].key < start {
+		for current.forward[i] != nil && current.forward[i].content < start {
 			current = current.forward[i]
 		}
 	}
 
 	current = current.forward[0]
 
-	for current != nil && current.key <= end {
+	for current != nil && current.content <= end {
 		count++
 		current = current.forward[0]
 	}
@@ -140,14 +138,14 @@ func (sl *SkipList) RangeCount(start, end int) int {
 	return count
 }
 
-// Delete 删除指定 key 的节点
-func (sl *SkipList) Delete(key int) bool {
+// Delete 删除指定 content 的节点
+func (sl *SkipList) Delete(content int64) bool {
 	update := make([]*node, maxLevel)
 	current := sl.head
 
 	// 寻找每层删除位置
 	for i := sl.level - 1; i >= 0; i-- {
-		for current.forward[i] != nil && current.forward[i].key < key {
+		for current.forward[i] != nil && current.forward[i].content < content {
 			current = current.forward[i]
 		}
 		update[i] = current
@@ -155,7 +153,7 @@ func (sl *SkipList) Delete(key int) bool {
 
 	current = current.forward[0]
 
-	if current != nil && current.key == key {
+	if current != nil && current.content == content {
 		// 逐层删除
 		for i := 0; i < sl.level; i++ {
 			if update[i].forward[i] != current {
@@ -177,13 +175,13 @@ func (sl *SkipList) Delete(key int) bool {
 }
 
 // RangeDelete 删除范围 [start, end] 内的节点
-func (sl *SkipList) RangeDelete(start, end int) {
+func (sl *SkipList) RangeDelete(start, end int64) {
 	update := make([]*node, maxLevel)
 	current := sl.head
 
 	// 寻找每层删除位置
 	for i := sl.level - 1; i >= 0; i-- {
-		for current.forward[i] != nil && current.forward[i].key < start {
+		for current.forward[i] != nil && current.forward[i].content < start {
 			current = current.forward[i]
 		}
 		update[i] = current
@@ -192,7 +190,7 @@ func (sl *SkipList) RangeDelete(start, end int) {
 	current = current.forward[0]
 
 	// 删除范围内的节点
-	for current != nil && current.key <= end {
+	for current != nil && current.content <= end {
 		// 逐层删除
 		for i := 0; i < sl.level; i++ {
 			if update[i].forward[i] != current {
---
+++ plugins/wasm-go/extensions/cc/ZSet.go
@@ -0,0 +1,46 @@
+package main
+
+type ZSet struct {
+	data map[string]*SkipList
+}
+
+func (z *ZSet) ZAdd(key string, score int64) {
+	if z.data == nil {
+		z.data = make(map[string]*SkipList)
+	}
+
+	sl, found := z.data[key]
+	if !found {
+		sl = NewSkipList()
+		z.data[key] = sl
+	}
+
+	sl.Insert(score)
+}
+
+func (z *ZSet) ZCount(key string, min, max int64) int {
+	sl, found := z.data[key]
+	if !found {
+		return 0
+	}
+
+	return sl.RangeCount(min, max)
+}
+
+func (z *ZSet) ZRange(key string, min, max int64) []int64 {
+	sl, found := z.data[key]
+	if !found {
+		return []int64{}
+	}
+
+	return sl.RangeSearch(min, max)
+}
+
+func (z *ZSet) ZRemByScore(key string, min, max int64) {
+	sl, found := z.data[key]
+	if !found {
+		return
+	}
+
+	sl.RangeDelete(min, max)
+}
