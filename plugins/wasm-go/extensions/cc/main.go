4d256602048637b8cecd4d6cde88f90a58185bb6
zhoulaiqing <zhoulaiqing@hotmail.com>
2023-07-27 23:12:00 +0800

    CCConfig data structure

--- plugins/wasm-go/extensions/cc/main.go
+++ plugins/wasm-go/extensions/cc/main.go
@@ -1,10 +1,14 @@
 package main
 
-//func main() {
-//	fmt.Println("Hello world.")
-//}
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world.")
+}
 
 type CCConfig struct {
+	headerRulesMap map[string][]CCRule
+	cookieRulesMap map[string][]CCRule
 }
 
 type CCRule struct {
