ce7ed34601e109a3552225e96aaef2661100a626
zhoulaiqing <zhoulaiqing@baicizhan.com>
2023-08-29 17:29:31 +0800

    more rules in engine

--- plugins/wasm-go/extensions/waf-deny/wasmplugin/go_rules/rule_engine.go
+++ plugins/wasm-go/extensions/waf-deny/wasmplugin/go_rules/rule_engine.go
@@ -4,6 +4,12 @@ import (
 	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/core"
 	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/go_rules/rule_920"
 	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/go_rules/rule_932"
+	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/go_rules/rule_933"
+	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/go_rules/rule_934"
+	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/go_rules/rule_941"
+	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/go_rules/rule_942"
+	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/go_rules/rule_943"
+	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/go_rules/rule_944"
 	"github.com/corazawaf/coraza-proxy-wasm/wasmplugin/rule_tasks"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 )
@@ -23,7 +29,28 @@ var (
 		&Rule922100{}, &Rule922110_120{},
 		&Rule930100{}, &Rule930110{}, &Rule930120{}, &Rule930130{},
 		&Rule931100{}, &Rule931110{}, &Rule931120{},
-		&rule_932.Rule932230{}, &RuleFinal{},
+		&rule_932.Rule932230{}, &rule_932.Rule932115{}, &rule_932.Rule932120{}, &rule_932.Rule932125{}, &rule_932.Rule932130{},
+		&rule_932.Rule932140{}, &rule_932.Rule932160{}, &rule_932.Rule932170{}, &rule_932.Rule932171{}, &rule_932.Rule932175{},
+		&rule_932.Rule932180{}, &rule_932.Rule932230{}, &rule_932.Rule932235{}, &rule_932.Rule932250{}, &rule_932.Rule932260{},
+		&rule_932.Rule932330{}, &rule_932.Rule932370{},
+		&rule_933.Rule933100{}, &rule_933.Rule933110{}, &rule_933.Rule933120{}, &rule_933.Rule933130{}, &rule_933.Rule933140{},
+		&rule_933.Rule933150{}, &rule_933.Rule933160{}, &rule_933.Rule933170{}, &rule_933.Rule933180{}, &rule_933.Rule933200{},
+		&rule_933.Rule933210{},
+		&rule_934.Rule934100{}, &rule_934.Rule934101{}, &rule_934.Rule934110{}, &rule_934.Rule934130{}, &rule_934.Rule934150{},
+		&rule_934.Rule934160{}, &rule_934.Rule934170{},
+		&rule_941.Rule941010{}, &rule_941.Rule941100{}, &rule_941.Rule941110{}, &rule_941.Rule941130{}, &rule_941.Rule941140{},
+		&rule_941.Rule941160{}, &rule_941.Rule941170{}, &rule_941.Rule941180{}, &rule_941.Rule941190{}, &rule_941.Rule941200{},
+		&rule_941.Rule941210{}, &rule_941.Rule941220{}, &rule_941.Rule941230{}, &rule_941.Rule941240{}, &rule_941.Rule941250{},
+		&rule_941.Rule941260{}, &rule_941.Rule941270{}, &rule_941.Rule941280{}, &rule_941.Rule941290{}, &rule_941.Rule941300{},
+		&rule_941.Rule941310{}, &rule_941.Rule941350{}, &rule_941.Rule941360{}, &rule_941.Rule941370_400{}, &rule_941.Rule941390{},
+		&rule_942.Rule942100{}, &rule_942.Rule942140{}, &rule_942.Rule942151{}, &rule_942.Rule942160{}, &rule_942.Rule942170{},
+		&rule_942.Rule942190{}, &rule_942.Rule942220{}, &rule_942.Rule942230{}, &rule_942.Rule942240{}, &rule_942.Rule942250{},
+		&rule_942.Rule942270{}, &rule_942.Rule942280{}, &rule_942.Rule942290{}, &rule_942.Rule942320{}, &rule_942.Rule942350{},
+		&rule_942.Rule942360{}, &rule_942.Rule942500{}, &rule_942.Rule942540{}, &rule_942.Rule942550{},
+		&rule_943.Rule943100{}, &rule_943.Rule943110_120{},
+		&rule_944.Rule944100{}, &rule_944.Rule944110{}, &rule_944.Rule944120{}, &rule_944.Rule944130{}, &rule_944.Rule944140{},
+		&rule_944.Rule944150{},
+		&RuleFinal{},
 	}
 )
 
